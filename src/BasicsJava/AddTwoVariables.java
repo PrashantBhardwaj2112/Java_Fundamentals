e9e64625a095c6cc67a39c9eb883616d2973fbab
Prashant Bhardwaj <60250947+PrashantBhardwaj2112@users.noreply.github.com>
2026-06-29 23:32:25 +0530

    Initial commit

---
+++ src/BasicsJava/AddTwoVariables.java
@@ -0,0 +1,10 @@
+package BasicsJava;
+
+public class AddTwoVariables {
+    public static void main(String[] args) {
+        System.out.println(addfun(2,3));
+    }
+    public static int addfun(int a , int b){
+        return a+b;
+    }
+}
